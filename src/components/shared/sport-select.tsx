e72e4cfe8221d3608ad58bde4134c6579355ed76
Nick Weindel <nickweindel@proton.me>
2025-09-21 20:21:31 -0500

    add a bunch of new leagues

--- src/components/shared/sport-select.tsx
+++ src/components/shared/sport-select.tsx
@@ -14,7 +14,9 @@ interface SportSelectProps {
 
 export function SportSelect({ onChange } : SportSelectProps) {
   const publicSubDirectory = "league-logos";
-  const ncaaPath = `/${publicSubDirectory}/ncaa.png`
+  const basketballSubDirectory = "basketball";
+  const soccerSubDirectory = "soccer";
+  const ncaaPath = `/${publicSubDirectory}/ncaa.png`;
 
   const leagues = [
     {
@@ -40,17 +42,77 @@ export function SportSelect({ onChange } : SportSelectProps) {
     {
       value: 'nba',
       label: 'NBA',
-      imageSrc: `/${publicSubDirectory}/nba.png`,
+      imageSrc: `/${publicSubDirectory}/${basketballSubDirectory}/nba.png`,
+    },
+    {
+      value: 'wnba',
+      label: 'WNBA',
+      imageSrc: `/${publicSubDirectory}/${basketballSubDirectory}/wnba.png`,
     },
     {
       value: 'mens-college-basketball',
-      label: 'College Basketball',
+      label: "Men's College Basketball",
+      imageSrc: ncaaPath,
+    },
+    {
+      value: 'womens-college-basketball',
+      label: "Women's College Basketball",
       imageSrc: ncaaPath,
     },
     {
       value: 'arg.1',
       label: 'Argentine Primera División',
-      imageSrc: `/${publicSubDirectory}/argentine-primera.png`
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/arg.png`,
+    },
+    {
+      value: 'bra.1',
+      label: 'Brazilian Série A',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/bra.png`,
+    },
+    {
+      value: 'eng.1',
+      label: 'EPL',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/eng.png`,
+    },
+    {
+      value: 'esp.1',
+      label: 'La Liga',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/esp.png`,
+    },
+    {
+      value: 'fra.1',
+      label: 'Ligue 1',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/fra.png`,
+    },
+    {
+      value: 'ger.1',
+      label: 'Bundesliga',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/ger.png`,
+    },
+    {
+      value: 'ita.1',
+      label: 'Serie A',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/ita.png`,
+    },
+    {
+      value: 'mex.1',
+      label: 'Liga MX',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/mex.png`,
+    },
+    {
+      value: 'ned.1',
+      label: 'Eredivisie',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/ned.png`,
+    },
+    {
+      value: 'por.1',
+      label: 'Primeira Liga',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/por.png`,
+    },
+    {
+      value: 'usa.1',
+      label: 'MLS',
+      imageSrc: `/${publicSubDirectory}/${soccerSubDirectory}/usa.png`,
     }
   ]
 
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,22 +10,44 @@ export const API_PATH: string = "scoreboard";
 export const LEAGUE_TO_SPORT_MAPPING: Record<League, Sport[keyof Sport]> = {
     "college-football": "football",
     "nba": "basketball",
+    "wnba": "basketball",
     "nhl": "hockey",
     "mens-college-basketball": "basketball",
+    "womens-college-basketball": "basketball",
     "nfl": "football",
     "mlb": "baseball",
     "arg.1": "soccer",
+    "eng.1": "soccer",
+    "usa.1": "soccer",
+    "fra.1": "soccer",
+    "esp.1": "soccer",
+    "ned.1": "soccer",
+    "ita.1": "soccer",
+    "mex.1": "soccer",
+    "bra.1": "soccer",
+    "por.1": "soccer",
 }
 
 // League-to-venue mapping.
 export const LEAGUE_TO_VENUE_TYPE_MAPPING: Record<League, VenueType[keyof VenueType]> = {
     "college-football": "Stadiums",
     "nba": "Arenas",
+    "wnba": "Arenas",
     "nhl": "Arenas",
     "mens-college-basketball": "Arenas",
+    "womens-college-basketball": "Arenas",
     "nfl": "Stadiums",
     "mlb": "Stadiums",
     "arg.1": "Stadiums",
+    "eng.1": "Stadiums",
+    "usa.1": "Stadiums",
+    "fra.1": "Stadiums",
+    "esp.1": "Stadiums",
+    "ned.1": "Stadiums",
+    "ita.1": "Stadiums",
+    "mex.1": "Stadiums",
+    "bra.1": "Stadiums",
+    "por.1": "Stadiums",
 }
 
 // Leagues where we should display ties.
