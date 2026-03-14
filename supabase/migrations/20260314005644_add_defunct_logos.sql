0d1c9df39f1415af9f429e2667a654a3eb16d66a
Nick Weindel <nickweindel@proton.me>
2026-03-13 20:37:45 -0500

    wip - defunct team migration

---
+++ supabase/migrations/20260314005644_add_defunct_logos.sql
@@ -0,0 +1,25 @@
+-- Atlanta Thrashers
+INSERT INTO public.team_override (league, team_abbreviation, team_abbreviation_override, team_logo)
+VALUES ('nhl', 'ATL', NULL, 'https://sportslogohistory.com/wp-content/uploads/2017/12/atlanta_thrashers_2000-2011.png');
+
+-- Seattle SuperSonics
+INSERT INTO public.team_override (league, team_abbreviation, team_abbreviation_override, team_logo)
+VALUES ('nba', 'SEA', NULL, 'https://sportslogohistory.com/wp-content/uploads/2017/12/seattle_supersonics_2002-2008.png');
+
+-- Montreal Expos
+INSERT INTO public.team_override (league, team_abbreviation, team_abbreviation_override, team_logo)
+VALUES ('mlb', 'MON', NULL, 'https://sportslogohistory.com/wp-content/uploads/2017/12/montreal_expos_1992-2004.png');
+
+-- Florida Marlins
+INSERT INTO public.team_override (league, team_abbreviation, team_abbreviation_override, team_logo)
+VALUES ('mlb', 'FLA', NULL, 'https://sportslogohistory.com/wp-content/uploads/2017/12/florida_marlins_1993-2011.png');
+
+-- St Louis Rams
+INSERT INTO public.team_override (league, team_abbreviation, team_abbreviation_override, team_logo)
+VALUES ('nfl', 'STL', NULL, 'https://sportslogohistory.com/wp-content/uploads/2017/12/st_louis_rams_2000-2015.png');
+
+-- New Jersey Nets
+INSERT INTO public.team_override (league, team_abbreviation, team_abbreviation_override, team_logo)
+VALUES ('nba', 'NJ', NULL, 'https://sportslogohistory.com/wp-content/uploads/2016/SLH/nba_primary/new_jersey_nets.png');
+
+-- TODO: Arizona Coyotes, Phoenix Coyotes, San Diego Chargers
